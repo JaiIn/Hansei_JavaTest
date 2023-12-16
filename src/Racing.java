e5bf16510107e11ee64766e4a5b51ddbe4e425ee
[JaiIn] <[di0308@naver.com]>
2023-12-16 20:10:07 +0900

    5. 시작 페이지 끝
    ...

--- src/Racing.java
+++ src/Racing.java
@@ -7,7 +7,7 @@ public class Racing extends Frame {
 	private int Track = 100;
 	
 	Scanner sc = new Scanner(System.in);
-	int RacerNum = 0;
+	int RacerNum = 2;
 	
 	Racer[] Racers;
 	
@@ -22,19 +22,46 @@ public class Racing extends Frame {
 		
 		// 버튼 1개
 		/*
+		 	첫 창
 		 	1. 레이블로 위에 Racing! 띄우기
 		 	2. 시작 버튼 띄우기
-		 	3. 버튼을 누르면 Racer 생성창으로 이동
-		 	4. 추가, 삭제, 시작 버튼
-		 	5. 끝날때 까지 대기
-		 	6. 끝나면 우승자 말해주기
-		 	7. 닫기 버튼 활성화
+		 	3. Racer 생성 버튼
+		 	다음 창
+		 	4. 끝날때 까지 대기
+		 	5. 끝나면 우승자 말해주기
+		 	6. 닫기 버튼 활성화
 		*/
-		Label MainLabel = new Label("Welcome to Racing!", Label.CENTER);
+		Label MainLabel = new Label("Input How many Racer", Label.CENTER);
 		MainLabel.setBackground(Color.LIGHT_GRAY);
 		MainLabel.setBounds(100, 100, 200, 50);
-
+		
+		// Racer 생성 택스트 필드
+		TextField InputNum = new TextField(5);
+		InputNum.setBounds(150,210,50,30);
+		
+		Button Sub = new Button("Submit!");
+		Sub.setBounds(230, 200, 50, 50);
+		Sub.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				try {
+				RacerNum = Integer.parseInt(InputNum.getText());
+				if(2 <= RacerNum && RacerNum <= 5 )
+				{
+					Racers = new Racer[RacerNum];
+					for(int i =0; i<RacerNum;++i)
+					{Racers[i] = new Racer("No." + (i+1));}
+					MainLabel.setText("Setting "+Racers.length + " Racer");
+				}
+				else 
+				{
+					MainLabel.setText("Input 2 ~ 5, Defalut is 2");
+					RacerNum = 2;
+				}}catch(Exception e2) {MainLabel.setText("Input!");}
+			}
+		});
+		
 		Button SB = new Button("Start Racing!");
+		SB.setBounds(100, 300, 200, 50);
 		
 		SB.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -46,7 +73,8 @@ public class Racing extends Frame {
 			public void windowClosing(WindowEvent e) { dispose(); }	
 			});
 		
-		add(SB);
+		add(InputNum);
+		add(SB); add(Sub);
 		add(MainLabel);
 		setVisible(true);
 	}
