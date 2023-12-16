0f28dc9631e05ed2e7bb3aa1cb8c5e2f84c97293
[JaiIn] <[di0308@naver.com]>
2023-12-16 19:32:30 +0900

    4. 제일 처음 시작 메뉴
    ...

--- src/Racing.java
+++ src/Racing.java
@@ -4,10 +4,15 @@ import java.util.*;
 
 public class Racing extends Frame {
 	
+	private int Track = 100;
+	
+	Scanner sc = new Scanner(System.in);
+	int RacerNum = 0;
+	
+	Racer[] Racers;
+	
 	Toolkit tk = Toolkit.getDefaultToolkit();
 	Dimension screenSize = tk.getScreenSize();
-	
-	int RacerNum = 0;
 	
 	Racing(String title)
 	{
@@ -15,43 +20,45 @@ public class Racing extends Frame {
 		setLayout(null);
 		setBounds(screenSize.width / 2 - 200, screenSize.height / 2 - 200, 400, 400);
 		
+		// 버튼 1개
+		/*
+		 	1. 레이블로 위에 Racing! 띄우기
+		 	2. 시작 버튼 띄우기
+		 	3. 버튼을 누르면 Racer 생성창으로 이동
+		 	4. 추가, 삭제, 시작 버튼
+		 	5. 끝날때 까지 대기
+		 	6. 끝나면 우승자 말해주기
+		 	7. 닫기 버튼 활성화
+		*/
+		Label MainLabel = new Label("Welcome to Racing!", Label.CENTER);
+		MainLabel.setBackground(Color.LIGHT_GRAY);
+		MainLabel.setBounds(100, 100, 200, 50);
+
+		Button SB = new Button("Start Racing!");
 		
+		SB.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				BeforeStart();
+			}
+		});
 		
 		addWindowListener(new WindowAdapter() {
 			public void windowClosing(WindowEvent e) { dispose(); }	
 			});
 		
+		add(SB);
+		add(MainLabel);
 		setVisible(true);
 	}
-	
+	public void BeforeStart()
+	{
+		
+	}
 	public class Racer 
 	{
-		Toolkit tk = Toolkit.getDefaultToolkit();
-		Dimension screenSize = tk.getScreenSize();
-		
 		private double Speed = 5.0;
-		private String name = "Frist";
+		private String name = "1";
 		
-		public void setName(String n){this.name = n;}
-		public void setSpeed(double s)
-		{
-			if(0.0 < s && s <10.0){Speed = s;}
-			else 
-			{
-				Frame Warn = new Frame("Warning");
-				Warn.setBounds
-				(screenSize.width / 2 - 200, screenSize.height / 2 - 200, 300, 50);
-				
-				Label Wl = new Label("Input 0.0 ~ 10.0 !",Label.CENTER);
-				
-				Warn.add(Wl);
-				
-				Warn.addWindowListener(new WindowAdapter() {
-					public void windowClosing(WindowEvent e) { Warn.setVisible(false); }	
-					});
-				
-				Warn.setVisible(true);
-			}
-		}
+		Racer(String name){this.name = name;}
 	}
 }
